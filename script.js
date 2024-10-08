f1ebc64ed3a1f8727e3ca2cff07e43f921b2c909
lalit-machra <lalitmachra0403@gmail.com>
2024-10-08 10:10:56 +0530

    Add functionality to play the game with alternate turns
    Users can now play the game in alternate turns, filling up the cells they
    want. No game logic has been implemented yet.

--- script.js
+++ script.js
@@ -13,18 +13,22 @@ For starting just make them all empty.
 5) cover all the cases for 3 in a row, 3 in a column and 3 in a diagonal to decide the final winner.
 */
 
-console.log("Welcome to Tic Tac Toe");
+console.log("%c Welcome to Tic Tac Toe ", "background-color: red; color: white; font-size: 24px;");
 
-const players = {};
+const players = {
+  player1: "X",
+  player2: "O",
+};
 
 let gameBoard = {
-  "r1c1": "_", "r1c2": "_", "r1c3": "_",
-  "r2c1": "_", "r2c2": "_", "r2c3": "_",
-  "r3c1": "_", "r3c2": "_", "r3c3": "_",
+  r1c1: "_", r1c2: "_", r1c3: "_",
+  r2c1: "_", r2c2: "_", r2c3: "_",
+  r3c1: "_", r3c2: "_", r3c3: "_",
 }
 
 function showBoard(board) {
   const allCells = Object.keys(board);
+  console.log("<--------------------------->");
   for (let i = 0; i < 3; i++) {
     let a = (3 * i);
     console.log(`${board[allCells[a]]}   ${board[allCells[a + 1]]}   ${board[allCells[a + 2]]}`);
@@ -33,3 +37,28 @@ function showBoard(board) {
 }
 
 showBoard(gameBoard);
+
+function fillCell(playerSymbol, position) {
+  gameBoard[position] = playerSymbol;
+}
+
+
+function playGame() {
+  // For development phase, run the game for 9 turns to fill up entire board
+  let currPlayerSym, currPlayerCell;
+  for (let i = 0; i < 9; i++) {
+    if (i % 2 === 0) {
+      console.log("Player1's turn:");
+      currPlayerSym = players["player1"]
+    }
+    else {
+      console.log("Player2's turn:");
+      currPlayerSym = players["player2"];
+    }
+    currPlayerCell = prompt("Enter your target cell (e.g.- r2c3 target cell present in 2nd row at 3rd column):");
+    fillCell(currPlayerSym, currPlayerCell);
+    showBoard(gameBoard);
+  }
+}
+
+playGame()
